c72e01669e2459be78c7dccdd9d1c08fcda1a2fa
zyxkad <zyxkad@gmail.com>
2025-08-05 21:36:17 -0600

    fix ship serialization issue

--- common/src/main/java/com/github/litermc/vschunkloader/attachment/ForceLoadAttachment.java
+++ common/src/main/java/com/github/litermc/vschunkloader/attachment/ForceLoadAttachment.java
@@ -1,24 +1,24 @@
 package com.github.litermc.vschunkloader.attachment;
 
-import  com.github.litermc.vschunkloader.util.ResourceLocationCollectionSerializer;
-
 import net.minecraft.resources.ResourceLocation;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import org.valkyrienskies.core.api.ships.ServerShip;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
 @JsonAutoDetect(
-	fieldVisibility = JsonAutoDetect.Visibility.ANY,
+	fieldVisibility = JsonAutoDetect.Visibility.NONE,
+	isGetterVisibility = JsonAutoDetect.Visibility.NONE,
 	getterVisibility = JsonAutoDetect.Visibility.NONE,
-	isGetterVisibility = JsonAutoDetect.Visibility.NONE
+	setterVisibility = JsonAutoDetect.Visibility.NONE
 )
 public final class ForceLoadAttachment {
-	@JsonSerialize(using = ResourceLocationCollectionSerializer.class)
 	private Set<ResourceLocation> forceLoadTokens = new HashSet<>();
 
 	public ForceLoadAttachment() {}
@@ -54,5 +54,16 @@ public final class ForceLoadAttachment {
 
 	public Set<ResourceLocation> getAllForceLoadTokens() {
 		return Collections.unmodifiableSet(this.forceLoadTokens);
+	}
+
+	@JsonGetter("forceLoadTokens")
+	private Collection<String> getForceLoadTokens() {
+		return this.forceLoadTokens.stream().map(ResourceLocation::toString).toList();
+	}
+
+	@JsonSetter("forceLoadTokens")
+	private void setForceLoadTokens(final Collection<String> tokens) {
+		this.forceLoadTokens.clear();
+		tokens.stream().map(ResourceLocation::new).forEach(this.forceLoadTokens::add);
 	}
 }
