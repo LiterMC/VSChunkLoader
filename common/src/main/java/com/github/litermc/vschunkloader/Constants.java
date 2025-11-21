ee8e86360920287a065cde8dc80370316f1807bf
zyxkad <zyxkad@gmail.com>
2025-11-21 16:14:57 -0700

    bump version to 1.5.0

--- common/src/main/java/com/github/litermc/vschunkloader/Constants.java
+++ common/src/main/java/com/github/litermc/vschunkloader/Constants.java
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory;
 public final class Constants {
 	public static final String MOD_ID = "vschunkloader";
 	public static final String MOD_NAME = "VSChunkLoader";
-	public static final String MOD_VERSION = "1.4.0";
+	public static final String MOD_VERSION = "1.5.0";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 
 	private Constants() {}
